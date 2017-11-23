732f90fdb35855835aa12b138d635418cb76991e
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-11-23 15:57:35 +0700

    create room controller

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,67 @@
+class RoomController < ApplicationController
+
+  ## use a before action so you dont have to write set room under all the actions. we dont want it for index new and create as we dont have a room during those actions
+  before_action :set_room, except: [:index, :new, :create]
+
+  ## we want the user to be logged in for all actions apart from the show one because it is public to all users, even not logged in
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    ## we define the params below
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      redirect_to listing_room_path(@room), notice: "Saved..."
+    else
+      render new, notice: "Something went wrong..."
+    end
+  end
+
+  def show
+  end
+
+  def listing
+  end
+
+  def pricing
+  end
+
+  def description
+  end
+
+  def photo_upload
+  end
+
+  def amenities
+  end
+
+  def location
+  end
+
+  def update
+    if @room.update(room_params)
+      flash[:notice] = "Saved..."
+    else
+      flash[:notice] = "Something went wrong..."
+    end
+    ## redirect back to the page where we came from
+    redirect_back(fallback_location: request.referer)
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])    
+  end
+
+  def room_params
+    params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  
   root 'pages#home'
   devise_for :users,
-              path: '',
-              path_names: {sign_in: 'login', sign_out: 'logout', edit:'profile', sign_up: 'registration'},
-              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' },
+             controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources  :users, only: [:show]
+
+  
+  resources :rooms, except: [:edit] do
+    member do
+      ## member runs the action for the speific room id, thats why we have rooms/id/listing
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
