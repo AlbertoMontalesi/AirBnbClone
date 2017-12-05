28a7f5f14518e4e45b47cc0bc7cd74f7f8cb8e7e
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-12-05 20:44:23 +0700

    create reservation page

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,7 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
   get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
+  
+  
 end
